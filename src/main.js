564fd12a6fa9e4b0cde47810481571b0aaf3d64f
Subramanian Venkatesan <subramanian.bsv@gmail.com>
2018-09-13 00:26:05 +0530

    Simple UI

--- src/main.js
+++ src/main.js
@@ -30,12 +30,24 @@ function receiveMessage (event) {
   }
 }
 
+function fixContract() {
+  console.log('fix contract');
+}
+
+function analyseContract() {
+  console.log('ac');
+}
+
 window.addEventListener('message', receiveMessage, false);
 
 window.onload = function () {
+  document.getElementById('fix_button').addEventListener('click', fixContract);
+  document.getElementById('analyse_button').addEventListener('click', analyseContract);
+
   dispatch('request', 'editor', 'getCurrentFile', [], function (err, file) {
+    document.getElementById('contract_name').innerHTML = file[0];
     dispatch('request', 'editor', 'getFile', file, function (err, content) {
-      
+      console.log(content);
     });
   });
 }
